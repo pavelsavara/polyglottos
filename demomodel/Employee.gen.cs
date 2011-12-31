a765d4e8bd22a2764ad26357e14287e8c9f8f449
pavel.savara@gmail.com <pavel.savara@gmail.com@6f51f01b-90c6-5275-0a55-c5a774ef9ddb>
2011-12-31 11:03:18 +0000

    drop empty nodes

--- demomodel/Employee.gen.cs
+++
@@ -1,9 +0,0 @@
-// file was generated via Polyglottos Fluentator
-// http://code.google.com/p/polyglottos/ by Pavel Savara
-
-namespace demomodel
-{
-    static public class EmployeeExtensions
-    {
-    }
-}
